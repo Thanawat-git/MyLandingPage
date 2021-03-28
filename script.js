e53d780ce96f9e6a9ce9592aede1d6dbe4080f0e
Thanawat-git <buysai123gw@gmail.com>
2021-03-28 18:04:07 +0700

    first page

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// document.getElementsByClassName("box1").style.display = "none";
+document.getElementById("#box1").style.display = "none";
+function loadToShow() {
+    // alert("5555")
+    // document.getElementById("texttest").style.display = "block"
+}
+
+// loadToShow()
